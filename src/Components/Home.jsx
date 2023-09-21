15b6aa8e1bcfcc0639f7deb45083f3189f323dab
sakshihanumantpawar <112334150+sakshihanumantpawar@users.noreply.github.com>
2023-09-21 19:58:36 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/aaisaheb padmavati devi.pg";
+import image from "../images/aaisaheb padmavati devi.png";
 
 const imageAltText = "Sakshi pawar web devloper ";
 
